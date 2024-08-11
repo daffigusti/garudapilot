0b7b8dbcf5b694a46816988876e3d343ec01046f
Muammar Khadafi <daffigusti0890@gmail.com>
2024-08-11 08:02:20 +0700

    Change Connect Server & Sentry

--- system/sentry.py
+++ system/sentry.py
@@ -17,9 +17,9 @@ CRASHES_DIR = "/data/crashes/"
 
 class SentryProject(Enum):
   # python project
-  SELFDRIVE = "https://5ad1714d27324c74a30f9c538bff3b8d@o4505034923769856.ingest.us.sentry.io/4505034930651136"
+  SELFDRIVE = "https://6d7702eeff19493d84dd4204555bce96@o1183464.ingest.us.sentry.io/4505590718922752"
   # native project
-  SELFDRIVE_NATIVE = "https://5ad1714d27324c74a30f9c538bff3b8d@o4505034923769856.ingest.us.sentry.io/4505034930651136"
+  SELFDRIVE_NATIVE = "https://6d7702eeff19493d84dd4204555bce96@o1183464.ingest.us.sentry.io/4505590718922752"
 
 
 def report_tombstone(fn: str, message: str, contents: str) -> None:

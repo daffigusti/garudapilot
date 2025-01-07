92aef9999a280feaba28f75d2d5fc6b7c9c89da5
Muammar Khadafi <daffigusti0890@gmail.com>
2025-01-07 10:05:37 +0700

    change api to konik.ai

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-export ATHENA_HOST="wss://athena.springerelectronics.com"
-export API_HOST="https://api.springerelectronics.com"
+export ATHENA_HOST="wss://athena.konik.ai"
+export API_HOST="https://api.konik.ai"
 exec ./launch_chffrplus.sh
